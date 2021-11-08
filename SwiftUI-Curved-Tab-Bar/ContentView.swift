b8d024c9de4f407d36491e507700c30fdd2d3ae4
“Golden <golden.chimusinde@cassavasmartech.co.zw>
2021-11-08 08:35:54 +0200

    Initial Commit

---
+++ SwiftUI-Curved-Tab-Bar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Curved-Tab-Bar
+//
+//  Created by Golden Chimusinde on 8/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Curved-Tab-Bar/SwiftUI_Curved_Tab_BarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Curved_Tab_BarApp.swift
+//  SwiftUI-Curved-Tab-Bar
+//
+//  Created by Golden Chimusinde on 8/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Curved_Tab_BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
